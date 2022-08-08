fe9aaadd97228c58afcdbae0c4ccc2d55285e283
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-08-08 11:05:10 +0200

    added create token functionality

--- api/user/urls.py
+++ api/user/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 from user import views
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', views.create_user_view, name='create'),
+    path('token/', obtain_auth_token, name='token'),
 ]
